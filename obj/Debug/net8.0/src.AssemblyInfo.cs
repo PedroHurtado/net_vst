dc65286842dc8fbb6e8b69a7e6776944139854d7
Pedro Hurtado <pedrohurt@gmail.com>
2024-10-10 09:04:30 +0200

    docs

--- obj/Debug/net8.0/src.AssemblyInfo.cs
+++ obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975eb42c6e22e56a9e1bbd7d8f8303c22f44958e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5f150b08bfb77d119d44e112219168183af948")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
