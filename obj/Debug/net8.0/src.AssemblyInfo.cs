d0dcbad11b8ea03f67e96c03b2cf458fc9978ab9
Pedro Hurtado <pedrohurt@gmail.com>
2024-10-18 10:21:15 +0200

    cache

--- obj/Debug/net8.0/src.AssemblyInfo.cs
+++ obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc65286842dc8fbb6e8b69a7e6776944139854d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+341bd57f13bc1eda58200960b02d97a82c7dee05")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
